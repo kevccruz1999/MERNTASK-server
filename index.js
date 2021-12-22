ec3ce804fa6b0196ee80a2c2867ab266d272e3d5
kevccruz1999 <kevinccruz19@gmail.com>
2021-12-22 15:10:02 -0600

    Ajustando algunas cosas

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 //arrancar la app
-app.listen(port, ()=>{
+app.listen(port,()=>{
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
